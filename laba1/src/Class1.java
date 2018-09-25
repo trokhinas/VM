0b1d6aaba936e4f23834abd5edb8f08735787084
Nikita <potapovnik666@gmail.com>
2018-09-25 11:51:28 +0300

    Добавлены переменные.

---
+++ laba1/src/Class1.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by admin on 25.09.2018.
+ */
+public class Class1 {
+    ArrayList<Double> x_In = new ArrayList<>();
+    ArrayList<Double> y_In = new ArrayList<>();
+    ArrayList<Double> XX_In = new ArrayList<>();
+    double EPS;
+    int N;
+}
